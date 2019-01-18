b24e3d6badfb37619f8a069d6899dca56233d069
nicolangiano <nicolangiano@gmail.com>
2019-01-18 17:12:51 -0300

    Creado el YII para hacer la magic

---
+++ basic/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=Sys_cabañas',
+    'username' => 'coodesoft',
+    'password' => 'c00des0ft',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
